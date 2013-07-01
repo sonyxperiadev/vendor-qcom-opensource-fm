47abd376fa60845965192080fdb63606b8738892
Venkateshwarlu Domakonda <Venkateshwarlu@codeaurora.org>
2013-07-01 14:38:07 +0530

    fm: Move FM APP source tree to vendor project
    - FM APP source moved from packages/apps to vendor/qcom-opensource/
      repository
    - Remove META data change intent handler
    
    Change-Id: I669ee82b5ab2a3a4717387e4ce0c1452423497e9

--- Android.mk
+++ Android.mk
@@ -1,10 +1,11 @@
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_PATH:= $(call my-dir)
+LOCAL_DIR_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, qcom/fmradio)
 LOCAL_JNI_SHARED_LIBRARIES := libqcomfm_jni
 
 LOCAL_MODULE:= qcom.fmradio
@@ -12,4 +13,6 @@ LOCAL_MODULE:= qcom.fmradio
 include $(BUILD_JAVA_LIBRARY)
 
 include $(LOCAL_PATH)/jni/Android.mk
+LOCAL_PATH := $(LOCAL_DIR_PATH)
+include $(LOCAL_PATH)/fmapp/Android.mk
 endif # is-vendor-board-platform
