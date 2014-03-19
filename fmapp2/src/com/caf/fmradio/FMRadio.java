4965983d7f37b78f68e2a964729efa7ffe7d4b40
Satish Kodishala <skodisha@codeaurora.org>
2014-03-19 18:19:46 +0530

    FM: Fix the issue with Ellipsize
    When the string length is greater than button size, adding single "."
    instead of "...".
    
    Change-Id: I5b13d37bf2100bba9805d3d0cc98de8b6b82c937
    CRs-Fixed: 624375

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -2046,6 +2046,11 @@ public class FMRadio extends Activity
             String display = "";
             if (station != null) {
                display = station.getName();
+                Log.e(LOGTAG, "Venkat:before" + display );
+                if (display.length() > 6)
+		 display = display.substring(0,6)+"...";
+	       Log.e(LOGTAG, "Venkat: after" + display );
+		mPresetButtons[buttonIndex].setEllipsize(TextUtils.TruncateAt.END);
                mPresetButtons[buttonIndex].setText(display);
                mPresetButtons[buttonIndex].setTag(station);
                addedStations++;
