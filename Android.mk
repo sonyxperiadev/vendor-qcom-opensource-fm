78c7ae5d72bd1f60f0707d478715cb90980e5e39
Venkateshwarlu Domakonda <vdomak@codeaurora.org>
2015-04-20 14:41:10 +0530

    FM: Build libfmjni by default
    Removed the compilation dependency for libfmjni
    
    Change-Id: Ida0c22510aeef6c5bbf6307372154f998b2b72e9

--- Android.mk
+++ Android.mk
@@ -1,12 +1,13 @@
-ifneq ($(TARGET_USES_AOSP),true)
-
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 LOCAL_PATH:= $(call my-dir)
 LOCAL_DIR_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
+
+ifneq ($(TARGET_USES_AOSP),true)
+
+ifeq ($(BOARD_HAVE_QCOM_FM),true)
+ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 
 LOCAL_SRC_FILES := $(call all-java-files-under, qcom/fmradio)
 LOCAL_JNI_SHARED_LIBRARIES := libqcomfm_jni
@@ -20,9 +21,10 @@ LOCAL_PATH := $(LOCAL_DIR_PATH)
 include $(LOCAL_PATH)/fmapp2/Android.mk
 LOCAL_PATH := $(LOCAL_DIR_PATH)
 include $(LOCAL_PATH)/FMRecord/Android.mk
-LOCAL_PATH := $(LOCAL_DIR_PATH)
-include $(LOCAL_PATH)/libfm_jni/Android.mk
 endif # is-vendor-board-platform
 endif # BOARD_HAVE_QCOM_FM
 
 endif # Not (TARGET_USES_AOSP)
+
+LOCAL_PATH := $(LOCAL_DIR_PATH)
+include $(LOCAL_PATH)/libfm_jni/Android.mk
